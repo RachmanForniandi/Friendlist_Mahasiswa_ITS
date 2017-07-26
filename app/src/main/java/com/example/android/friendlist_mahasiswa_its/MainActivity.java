a6d3c074c7cfbff02af236f67ea6634eafcd3a8a
RachmanForniandi <rachmanforniandi@gmail.com>
2017-07-26 21:43:31 +0700

    Mulai Rekayasa API dengan Retrofit2 & Butterknife

--- app/src/main/java/com/example/android/friendlist_mahasiswa_its/MainActivity.java
+++ app/src/main/java/com/example/android/friendlist_mahasiswa_its/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.android.friendlist_mahasiswa_its;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.example.android.friendlist_mahasiswa_its.api.RegisterAPI;
+import com.example.android.friendlist_mahasiswa_its.model.Value;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String URL = "";
+    private RadioButton radioJK_Btn;
+    private ProgressDialog progress;
+    String nrp,nama,jurusan;
+
+    @BindView(R.id.radio_JenisKelamin) RadioGroup radioGroup;
+    @BindView(R.id.et_NRP) EditText editTextNRP;
+    @BindView(R.id.et_Nama)EditText editTextNama;
+    @BindView(R.id.et_Jurusan)EditText editTextJurusan;
+
+    @OnClick(R.id.btn_Daftar)void daftar(){
+        //utk menampilkan progress dialog
+        progress =new ProgressDialog(this);
+        progress.setCancelable(false);
+        progress.setMessage("Loading ...");
+        progress.show();
+
+        nrp = editTextNRP.getText().toString();
+        nama = editTextNama.getText().toString();
+        jurusan = editTextJurusan.getText().toString();
+
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        radioJK_Btn = (RadioButton)findViewById(selectedId);
+        String jenis_kelamin = radioJK_Btn.getText().toString();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RegisterAPI api = retrofit.create(RegisterAPI.class);
+        Call<Value> call = api.daftar(nrp,nama,jurusan,jenis_kelamin);
+
+        call.enqueue(new Callback<Value>() {
+            @Override
+            public void onResponse(Call<Value> call, Response<Value> response) {
+                String value = response.body().getValue();
+                String message = response.body().getMessage();
+                progress.dismiss();
+
+                if (value.equals("1")){
+                    Toast.makeText(MainActivity.this, message,Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, message,Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Value> call, Throwable t) {
+                t.getStackTrace();
+                progress.dismiss();
+                Toast.makeText(MainActivity.this,"Jaringan sedang error!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
